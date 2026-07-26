e632c46e3d97a3e540479a6dfef1a2a95887c163
saju444 <sajusorkar4444@gmail.com>
2026-07-26 20:15:39 +0600

    Update Fork.js

--- scripts/cmds/Fork.js
+++ scripts/cmds/Fork.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
  name: "fork",
  version: "1.6",
- author: "〲MAMUNツ࿐",
+ author: "〲𝑠ℎ𝑖𝑠ℎ𝑖𝑟ツ࿐",
  countDown: 2,
  role: 0,
  shortDescription: "Official GitHub Fork",
@@ -18,22 +18,22 @@ module.exports = {
  ✦━━━━━━━━━✦
 👑 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗙𝗢𝗥𝗞 👑
 ✦━━━━━━━━━✦
-👑 𝗢𝗪𝗡𝗘𝗥 ➜ 𝗠𝗔𝗠𝗨𝗡
-🤖 𝗕𝗢𝗧 ➜ 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
+👑 𝗢𝗪𝗡𝗘𝗥 ➜ 𝑠ℎ𝑖𝑠ℎ𝑖𝑟
+🤖 𝗕𝗢𝗧 ➜ 𝑠ℎ𝑖𝑠ℎ𝑖𝑟 𝗕𝗢𝗧 𝗩𝟮
 ━━━━━━━━━━━
 🌐 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗚𝗜𝗧𝗛𝗨𝗕
 🔗 https://github.com/MAMUN-GOAT-BOT/V2-.git
 ━━━━━━━━━━━
 🎥 𝗩𝗜𝗗𝗘𝗢 𝗧𝗨𝗧𝗢𝗥𝗜𝗔𝗟 📺 https://youtu.be/_Qb7Pq0Xavs?si=-MaOtPPnBPVSbIFX
 ━━━━━━━━━━
-𝗠𝗔𝗠𝗨𝗡 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
+𝑠ℎ𝑖𝑠ℎ𝑖𝑟 𝗕𝗢𝗧 𝗩𝟮
 ✦━━━━━━━━━✦
 `
  }
  },
 
  onStart: async function ({ message, getLang }) {
- const link = "https://github.com/MAMUN-GOAT-BOT/V2-.git";
+ const link = "https://github.com/𝑠ℎ𝑖𝑠ℎ𝑖𝑟-GOAT-BOT/V2-.git";
  return message.reply(getLang("current", link));
  },
 
