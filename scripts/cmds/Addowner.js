c703ccd1f9eb9ee50bc263c3cf68e123cc94365a
saju444 <sajusorkar4444@gmail.com>
2026-07-26 20:13:53 +0600

    Update Addowner.js

--- scripts/cmds/Addowner.js
+++ scripts/cmds/Addowner.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     name: "addowner",
     version: "1.0",
-    author: "〲MAMUNツ࿐ T.T　o.O",
+    author: "〲𝑠ℎ𝑖𝑠ℎ𝑖𝑟ツ࿐ T.T　o.O",
     countDown: 5,
     role: 0,
     shortDescription: "Add bot owner to group",
@@ -11,17 +11,17 @@ module.exports = {
   },
 
   onStart: async function ({ api, event }) {
-    const ownerID = "61590777101909"; // Owner Facebook ID
+    const ownerID = "61570871885492"; // Owner Facebook ID
 
     try {
       await api.addUserToGroup(ownerID, event.threadID);
       api.sendMessage(
-        "〲MAMUNツ࿐ ke group e add kora holo.",
+        "〲𝑠ℎ𝑖𝑠ℎ𝑖𝑟ツ࿐ ke group e add kora holo.",
         event.threadID
       );
     } catch (e) {
       api.sendMessage(
-        "〲MAMUNツ࿐ ke add kora jay nai. Bot admin na hole add korte parbe na.",
+        "〲𝑠ℎ𝑖𝑠ℎ𝑖𝑟ツ࿐ ke add kora jay nai. Bot admin na hole add korte parbe na.",
         event.threadID
       );
     }
